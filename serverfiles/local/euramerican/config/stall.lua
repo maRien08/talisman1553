7ede18baf7833cb166b0bdbc3addf620c408cac2
maRien08 <108826139+maRien08@users.noreply.github.com>
2025-04-29 18:13:22 +0300

    Delete serverfiles directory

--- serverfiles/local/euramerican/config/stall.lua
+++
@@ -1,8 +0,0 @@
-
-CanBuyWhenStall = 1 --摆摊时是否允许购买其它摊位的东西，1 : 允许, 0 : 不允许
-
-MaxSloganTextSize = 30 --摆摊招牌的最大字节长度
-
-
-
-
--- serverfiles/script/war.lua
+++
@@ -1,9 +0,0 @@
-
-include("script/game_script_base.lua")
-
---城战的怪物被创建后调用,包括载入存盘后的城战
-function ResetMonsterData(creId, lev)
-        --OutputLog("ResetMonsterData "..lev)
-	--SetMonsterLev(creId, lev)
-	SetNpcLevAndAttrib(creId, lev)
-end
